afe07d8b4b231094183ad64b10fa1bcda3806d8f
Vladimir Peter <vlad.edna@gmail.com>
2021-02-05 23:37:13 +0800

    initialized new project sellmycar

--- projects/Blog/app/src/main/java/com/dev_vlad/blog/utils/DateUtil.kt
+++ projects/Blog/app/src/main/java/com/dev_vlad/blog/utils/DateUtil.kt
@@ -1,4 +1,36 @@
 package com.dev_vlad.blog.utils
 
-class DateUtil {
+import com.google.firebase.Timestamp
+import java.text.SimpleDateFormat
+import java.util.*
+
+object DateUtil{
+
+
+     //input string: YYYY-MM-DD HH:mm:ss
+    //output string: YYYY-MM-DD
+     fun removeTimeFromDateString(dateAsString: String) : String{
+         return dateAsString.substring(0, dateAsString.indexOf(" "))
+     }
+
+
+    fun fireStoreTimestampToString(timestamp: Timestamp):String{
+        val dateFormat: SimpleDateFormat = SimpleDateFormat("y-MM-DD HH:mm:ss", Locale.getDefault())
+
+         return dateFormat.format(timestamp.toDate())
+    }
+
+    fun dateStringToFireStoreTimestamp(dateAsString: String):Timestamp{
+        val dateFormat: SimpleDateFormat = SimpleDateFormat("y-MM-DD HH:mm:ss", Locale.getDefault())
+
+        return Timestamp(dateFormat.parse(dateAsString)!!)
+    }
+
+    fun getCurrentTimestampAsStr(): String{
+        val dateFormat: SimpleDateFormat = SimpleDateFormat("y-MM-DD HH:mm:ss", Locale.getDefault())
+
+        return dateFormat.format(Date())
+    }
+
+
 }
